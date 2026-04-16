544a6b34ef66cbf835b814ed12092af78063967c
shadrack mason <shadrack.mason@student.moringaschool.com>
2026-04-16 16:29:32 +0300

    intial commit

--- index.js
+++ index.js
@@ -1,5 +1,53 @@
 //Write your code here
+// Task 2: Declare and Initialize Attendee Object
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+};
 
+// Task 3: Log Attendee Name
+function logAttendeeName(attendee) {
+  console.log(attendee.name);
+}
+
+// Task 4: Log Ticket Price
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice);
+}
+
+// Task 5: Update Ticket Type
+function updateTicketType(attendee, newTicketType) {
+  attendee.ticketType = newTicketType;
+}
+
+// Task 6: Update Ticket Price
+function updateTicketPrice(attendee, newPrice) {
+  attendee.ticketPrice = newPrice;
+}
+
+// Task 7: Remove Event Property
+function removeEventProperty(attendee) {
+  delete attendee.event;
+}
+
+// Task 8: Add Checked-in Property
+function addCheckedInProperty(attendee) {
+  attendee.checkedIn = true;
+}
+
+// Export (VERY IMPORTANT for tests)
+module.exports = {
+  attendee,
+  logAttendeeName,
+  logTicketPrice,
+  updateTicketType,
+  updateTicketPrice,
+  removeEventProperty,
+  addCheckedInProperty
+};
 
 
 //Needed for the tests to work. Don't modify
